37d1694630cc2165f3b46c7865b593f8127508d3
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-07-29 10:18:12 +0200

    First commit

---
+++ js/console.js
@@ -0,0 +1,53 @@
+/* MOBILE CONSOLE OBJECT */
+function getMobileConsole() {
+  var config = studio.mobile = {
+    saveOnClose: true, // save on the clipboard the console contents
+    console: [],
+    getMobileConsoleFromStorage: function recoverMobileConsoleFromStorage() {
+      var console = [];
+      if (studio.extension.storage.getItem('mobileConsole')) {
+        try {
+          console = JSON.parse(studio.extension.storage.getItem('mobileConsole'));
+        } catch (e) {}
+      }
+      if (this.saveOnClose && console.length == 0) {
+        if (studio.readClipboard('mobileConsole').length > 0) {
+          try {
+            console = JSON.parse(studio.readClipboard('mobileConsole'));
+          } catch (e) {}
+        }
+      }
+      this.console = console;
+    },
+    saveMobileConsoleToStorage: function saveMobileConsoleToStorage() {
+      studio.extension.storage.setItem('mobileConsole', JSON.stringify(this.console));
+      if (this.saveOnClose && this.console.length > 0) {
+        try {
+          studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
+        } catch (e) {}
+      }
+    },
+    appendConsoleMessage: function appendConsoleMessage(message, type) {
+      if (message && message.length > 0) {
+        // append the message in pending
+        this.console.push({
+          message: message,
+          type: type || null,
+          date: new Date().getTime()
+        });
+      }
+      this.saveMobileConsoleToStorage();
+    },
+    clearConsoleMessages: function clearConsoleMessages() {
+      this.console = [];
+      studio.extension.storage.removeItem('mobileConsole');
+      if(this.saveOnClose) {
+        studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
+      }
+    }
+  }
+  return config;
+}
+if(typeof exports != 'undefined'){
+  exports.getConsole = getMobileConsole;
+}
