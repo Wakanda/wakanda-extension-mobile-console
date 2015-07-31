dbfaf06547a2dfa3f6058f14cceac7651a04391d
Jonathan Argentiero <jonathan.argentiero@gmail.com>
2015-07-31 10:54:57 +0200

    code review - styling
    tab indentation and function format

--- js/console.js
+++ js/console.js
@@ -1,63 +1,63 @@
 /* MOBILE CONSOLE OBJECT */
 function getMobileConsole() {
-  var config = studio.mobile = {
-    saveOnClose: true, // save on the clipboard the console contents
-    console: [],
-    consoleLog: [],
-    getMobileConsoleFromStorage: function recoverMobileConsoleFromStorage() {
-      var console = [];
-      if (studio.extension.storage.getItem('mobileConsole')) {
-        try {
-          console = JSON.parse(studio.extension.storage.getItem('mobileConsole'));
-        } catch (e) {
-          studio.alert('WARNING! the console log could not be read from the storage: '+String(e));
+    var config = studio.mobile = {
+        saveOnClose: true, // save on the clipboard the console contents
+        console: [],
+        consoleLog: [],
+        getMobileConsoleFromStorage: function() {
+            var console = [];
+            if (studio.extension.storage.getItem('mobileConsole')) {
+                try {
+                    console = JSON.parse(studio.extension.storage.getItem('mobileConsole'));
+                } catch (e) {
+                    studio.alert('WARNING! the console log could not be read from the storage: ' + String(e));
+                }
+            }
+            if (this.saveOnClose && console.length == 0) {
+                if (studio.readClipboard('mobileConsole').length > 0) {
+                    try {
+                        console = JSON.parse(studio.readClipboard('mobileConsole'));
+                    } catch (e) {
+                        studio.alert('WARNING! the console log could not be read from the clipboard: ' + String(e));
+                    }
+                }
+            }
+            this.console = console;
+        },
+        saveMobileConsoleToStorage: function() {
+            studio.extension.storage.setItem('mobileConsole', JSON.stringify(this.console));
+            if (this.saveOnClose && this.console.length > 0) {
+                try {
+                    studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
+                } catch (e) {
+                    studio.alert('WARNING! the console log could not be saved in the clipboard: ' + String(e));
+                }
+            }
+        },
+        appendConsoleMessage: function(message, type, category) {
+            if (message && message.length > 0) {
+                // append the message in pending
+                this.console.push({
+                    message: message,
+                    type: type || null,
+                    category: category || 'env',
+                    date: new Date().getTime()
+                });
+            }
+            this.saveMobileConsoleToStorage();
+        },
+        clearConsoleMessages: function() {
+            this.console = [];
+            studio.extension.storage.removeItem('mobileConsole');
+            if (this.saveOnClose) {
+                studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
+            }
         }
-      }
-      if (this.saveOnClose && console.length == 0) {
-        if (studio.readClipboard('mobileConsole').length > 0) {
-          try {
-            console = JSON.parse(studio.readClipboard('mobileConsole'));
-          } catch (e) {
-            studio.alert('WARNING! the console log could not be read from the clipboard: '+String(e));
-          }
-        }
-      }
-      this.console = console;
-    },
-    saveMobileConsoleToStorage: function saveMobileConsoleToStorage() {
-      studio.extension.storage.setItem('mobileConsole', JSON.stringify(this.console));
-      if (this.saveOnClose && this.console.length > 0) {
-        try {
-          studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
-        } catch (e) {
-          studio.alert('WARNING! the console log could not be saved in the clipboard: '+String(e));
-        }
-      }
-    },
-    appendConsoleMessage: function appendConsoleMessage(message, type, category) {
-      if (message && message.length > 0) {
-        // append the message in pending
-        this.console.push({
-          message: message,
-          type: type || null,
-          category: category || 'env',
-          date: new Date().getTime()
-        });
-      }
-      this.saveMobileConsoleToStorage();
-    },
-    clearConsoleMessages: function clearConsoleMessages() {
-      this.console = [];
-      studio.extension.storage.removeItem('mobileConsole');
-      if (this.saveOnClose) {
-        studio.writeClipboard('mobileConsole', JSON.stringify(this.console));
-      }
     }
-  }
-  return config;
+    return config;
 }
 // CHECKS FOR ENVIRONMENT
 if (typeof exports != 'undefined') {
-  // BACKEND ENVIRONMENT
-  exports.getConsole = getMobileConsole;
+    // BACKEND ENVIRONMENT
+    exports.getConsole = getMobileConsole;
 }
--- js/scripts.js
+++ js/scripts.js
@@ -4,148 +4,148 @@ studio.mobile.getMobileConsoleFromStorage();
 studio.mobile.consoleIndex = studio.mobile.console.length;
 
 function generateLogString(log) {
-  var date = new Date(log.date).toLocaleTimeString();
-  var logHTML = '',
-    logCategory = '';
-  switch (log.category) {
-    case 'env':
-      logCategory = 'Environment';
-      break;
-    case 'build':
-      logCategory = 'Build';
-      break;
-    case 'backend':
-      logCategory = 'Backend';
-      break;
-    default:
-      logCategory = '?';
-      break;
-  };
-  switch (log.type) {
-    case 'DEBUG':
-      logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type debug">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
-      break;
-    case 'WARNING':
-      logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type orange">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span><span class="orange">' + log.message + '</span></div>';
-      break;
-    case 'ERROR':
-      logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type red">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span><span class="red">' + log.message + '</span></div>';
-      break;
-    case 'LOG':
-      logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
-      break;
-    default:
-      logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
-      break;
-  }
-  return logHTML;
+    var date = new Date(log.date).toLocaleTimeString();
+    var logHTML = '',
+        logCategory = '';
+    switch (log.category) {
+        case 'env':
+            logCategory = 'Environment';
+            break;
+        case 'build':
+            logCategory = 'Build';
+            break;
+        case 'backend':
+            logCategory = 'Backend';
+            break;
+        default:
+            logCategory = '?';
+            break;
+    };
+    switch (log.type) {
+        case 'DEBUG':
+            logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type debug">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
+            break;
+        case 'WARNING':
+            logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type orange">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span><span class="orange">' + log.message + '</span></div>';
+            break;
+        case 'ERROR':
+            logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type red">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span><span class="red">' + log.message + '</span></div>';
+            break;
+        case 'LOG':
+            logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type">[' + log.type + ']</span> <span class="time">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
+            break;
+        default:
+            logHTML = '<div class="message"><span class="category">[' + logCategory + ']</span><span class="type">' + date.substring(0, 8) + '</span>' + log.message + '</div>';
+            break;
+    }
+    return logHTML;
 }
 
 function checkLogConsole() {
-  var consoleLog = studio.getConsoleLog();
-  for (var i = 0; i < consoleLog.length; i++) {
-    studio.mobile.appendConsoleMessage(consoleLog[i], 'LOG', 'backend');
-  }
+    var consoleLog = studio.getConsoleLog();
+    for (var i = 0; i < consoleLog.length; i++) {
+        studio.mobile.appendConsoleMessage(consoleLog[i], 'LOG', 'backend');
+    }
 }
 
 function checkMobileConsole() {
-  studio.mobile.getMobileConsoleFromStorage();
-  if (studio.mobile.console.length > 0) {
-    for (var i = studio.mobile.consoleIndex; i < studio.mobile.console.length; i++) {
-      var string = generateLogString(studio.mobile.console[i]);
-      document.getElementById('console-body-all').innerHTML += string;
-      document.getElementById('console-body-' + String(studio.mobile.console[i].category)).innerHTML += string;
-      studio.mobile.consoleIndex++;
+    studio.mobile.getMobileConsoleFromStorage();
+    if (studio.mobile.console.length > 0) {
+        for (var i = studio.mobile.consoleIndex; i < studio.mobile.console.length; i++) {
+            var string = generateLogString(studio.mobile.console[i]);
+            document.getElementById('console-body-all').innerHTML += string;
+            document.getElementById('console-body-' + String(studio.mobile.console[i].category)).innerHTML += string;
+            studio.mobile.consoleIndex++;
+        }
     }
-  }
 }
 
 function restoreMobileConsole() {
-  studio.mobile.getMobileConsoleFromStorage();
-  if (studio.mobile.console.length > 0) {
-    var elements = document.getElementsByClassName('messages-container');
-    for (var i in elements) {
-      if (elements.hasOwnProperty(i)) {
-        elements[i].innerHTML = '';
-      }
+    studio.mobile.getMobileConsoleFromStorage();
+    if (studio.mobile.console.length > 0) {
+        var elements = document.getElementsByClassName('messages-container');
+        for (var i in elements) {
+            if (elements.hasOwnProperty(i)) {
+                elements[i].innerHTML = '';
+            }
+        }
+        studio.mobile.consoleIndex = 0;
+        studio.mobile.console.forEach(function(log) {
+            var string = generateLogString(studio.mobile.console[studio.mobile.consoleIndex]);
+            document.getElementById('console-body-all').innerHTML += string;
+            document.getElementById('console-body-' + String(studio.mobile.console[studio.mobile.consoleIndex].category)).innerHTML += string;
+            studio.mobile.consoleIndex++;
+        });
     }
-    studio.mobile.consoleIndex = 0;
-    studio.mobile.console.forEach(function(log) {
-      var string = generateLogString(studio.mobile.console[studio.mobile.consoleIndex]);
-      document.getElementById('console-body-all').innerHTML += string;
-      document.getElementById('console-body-' + String(studio.mobile.console[studio.mobile.consoleIndex].category)).innerHTML += string;
-      studio.mobile.consoleIndex++;
-    });
-  }
 }
 
 function changeTab(tabName) {
-  var elements = document.getElementsByClassName('messages-tab');
-  for (var i in elements) {
-    if (elements.hasOwnProperty(i)) {
-      elements[i].className = 'messages-tab';
+    var elements = document.getElementsByClassName('messages-tab');
+    for (var i in elements) {
+        if (elements.hasOwnProperty(i)) {
+            elements[i].className = 'messages-tab';
+        }
     }
-  }
-  document.getElementById('console-tab-' + tabName).className = 'messages-tab active';
-  elements = document.getElementsByClassName('messages-container');
-  for (var i in elements) {
-    if (elements.hasOwnProperty(i) && elements[i].style) {
-      elements[i].style.display = '';
+    document.getElementById('console-tab-' + tabName).className = 'messages-tab active';
+    elements = document.getElementsByClassName('messages-container');
+    for (var i in elements) {
+        if (elements.hasOwnProperty(i) && elements[i].style) {
+            elements[i].style.display = '';
+        }
     }
-  }
-  document.getElementById('console-body-' + tabName).style.display = 'block';
+    document.getElementById('console-body-' + tabName).style.display = 'block';
 }
 
 function clearMobileConsole() {
-  studio.sendCommand('MobileConsole.clear');
-  studio.mobile.consoleIndex = 0;
-  var elements = document.getElementsByClassName('messages-container');
-  for (var i in elements) {
-    if (elements.hasOwnProperty(i)) {
-      elements[i].innerHTML = '';
+    studio.sendCommand('MobileConsole.clear');
+    studio.mobile.consoleIndex = 0;
+    var elements = document.getElementsByClassName('messages-container');
+    for (var i in elements) {
+        if (elements.hasOwnProperty(i)) {
+            elements[i].innerHTML = '';
+        }
     }
-  }
 }
 
 function toggleOptions(mode) {
-  if (mode == true) {
-    document.getElementById('options-container').style.display = 'block';
-  } else if (mode == false) {
-    document.getElementById('options-container').style.display = 'none';
-  } else {
-    if (document.getElementById('options-container').style.display && document.getElementById('options').style.display == 'block') {
-      document.getElementById('options-container').style.display = 'none';
+    if (mode == true) {
+        document.getElementById('options-container').style.display = 'block';
+    } else if (mode == false) {
+        document.getElementById('options-container').style.display = 'none';
     } else {
-      document.getElementById('options-container').style.display = 'block';
+        if (document.getElementById('options-container').style.display && document.getElementById('options').style.display == 'block') {
+            document.getElementById('options-container').style.display = 'none';
+        } else {
+            document.getElementById('options-container').style.display = 'block';
+        }
     }
-  }
 }
 
 function toggleTime() {
-  if (document.getElementById('toggleTime').className == 'switch') {
-    document.getElementById('toggleTime').className = 'switch active';
-    document.getElementById('console').classList.add("time");
-  } else {
-    document.getElementById('toggleTime').className = 'switch';
-    document.getElementById('console').classList.remove("time");
-  }
+    if (document.getElementById('toggleTime').className == 'switch') {
+        document.getElementById('toggleTime').className = 'switch active';
+        document.getElementById('console').classList.add("time");
+    } else {
+        document.getElementById('toggleTime').className = 'switch';
+        document.getElementById('console').classList.remove("time");
+    }
 }
 
 function toggleType() {
-  if (document.getElementById('toggleType').className == 'switch') {
-    document.getElementById('toggleType').className = 'switch active';
-    document.getElementById('console').classList.add("debug");
-  } else {
-    document.getElementById('toggleType').className = 'switch';
-    document.getElementById('console').classList.remove("debug");
-  }
+    if (document.getElementById('toggleType').className == 'switch') {
+        document.getElementById('toggleType').className = 'switch active';
+        document.getElementById('console').classList.add("debug");
+    } else {
+        document.getElementById('toggleType').className = 'switch';
+        document.getElementById('console').classList.remove("debug");
+    }
 }
 // ON START CHECK IF CONSOLE HAS MESSAGES
 if (document.getElementById('console-body-all').innerHTML == '') {
-  restoreMobileConsole();
+    restoreMobileConsole();
 }
 // CHECK PERIODICALLY IF CONSOLE HAS MESSAGES
 setInterval(function() {
-  checkLogConsole();
-  checkMobileConsole();
+    checkLogConsole();
+    checkMobileConsole();
 }, 250);
